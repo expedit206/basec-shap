38f27f02cc6f4bc3b53c25f5a5cc11312a3e3790
Expedit206 <mciagnessi@gmail.com>
2024-12-07 14:12:27 +0100

    mes premiers bases en cshap

--- obj/Debug/net8.0/first.AssemblyInfo.cs
+++ obj/Debug/net8.0/first.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee2f6f51d5de26c38099ab93f27fc0c4cfe3972")]
 [assembly: System.Reflection.AssemblyProductAttribute("first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
